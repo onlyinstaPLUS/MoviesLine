9929829bd7562c7218a29bc9e75d8ff2122caef5
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2023-12-17 11:49:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sowon16563:sowon16563@cluster0.1smt910.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "sowon16563")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://spushkar812:spushkar812@cluster0.friai7z.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "spushkar812")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
